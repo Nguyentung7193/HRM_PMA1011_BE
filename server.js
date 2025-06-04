f0b4d1c4a6ffe9bfe4867d4d6bb5791b2c872989
tungnxph19782 <tungnxph19782@gmail.com>
2025-06-04 10:05:17 +0700

    base 1

---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth/auth.router');
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB error:", err));
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express + MongoDB!');
+});
+
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
+});
