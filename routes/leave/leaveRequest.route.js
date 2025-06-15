395c43bfb719130219c31016ff8349a858d60f6c
duongnd <ducduong.contact@gmail.com>
2025-06-15 20:12:14 +0700

    15/06 handle leave request

--- routes/leave/leaveRequest.route.js
+++ routes/leave/leaveRequest.route.js
@@ -10,6 +10,9 @@ router.get('/:id', leaveRequestController.getLeaveRequestDetail);
 router.delete('/:id', leaveRequestController.deleteLeaveRequest);
 router.put('/:id', leaveRequestController.updateLeaveRequest);
 // router cho admin
-router.get('/admin/all',authenticate, leaveRequestController.getAllEmployeeLeaveRequests);
+router.get('/admin/all', authenticate, leaveRequestController.getAllEmployeeLeaveRequests);
 router.get('/admin/details/:id', authenticate, leaveRequestController.getLeaveRequestDetailAdmin);
+// Admin routes for approval/rejection
+router.post('/admin/approve/:id', authenticate, leaveRequestController.approveLeaveRequest);
+router.post('/admin/reject/:id', authenticate, leaveRequestController.rejectLeaveRequest);
 module.exports = router;
